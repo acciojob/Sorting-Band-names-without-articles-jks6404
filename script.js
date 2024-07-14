7e54245d5a1b8d9e96bde3e68f3bf79eacfa6f0b
Janu Kumar Singh <119485859+jks6404@users.noreply.github.com>
2024-07-14 09:51:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-//your code here
-//your code here
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-touristSpots.sort((a,b)=>{
-    let nameA = a.toLowerCase().substring(2); // ignorer upperCase
-    let nameB = b.toLowerCase().substring(2); // ignorer upperCase
-    if (nameA < nameB) {
-        return -1;
-    }
-    if (nameA > nameB) {
-    return 1;
-    }
 
-    // names must be equal
-    return 0;
-})
+function stripArticle(name) {
+    const articles = ["a ", "an ", "the "];
+    let lowerCaseName = name.toLowerCase();
+    
+    for (let article of articles) {
+        if (lowerCaseName.startsWith(article)) {
+            return name.slice(article.length).trim();
+        }
+    }
+    
+    return name.trim();
+}
+
+let touristSort = touristSpots.sort((a, b) => {
+    let nameA = stripArticle(a).toLowerCase();
+    let nameB = stripArticle(b).toLowerCase();
+    return nameA.localeCompare(nameB);
+});
+
 // Generate the list items
 const ul = document.getElementById('bands');
-touristSpots.forEach((bandName) => {
-  const li = document.createElement('li');
-  li.textContent = bandName;
-  ul.appendChild(li);
+touristSort.forEach((bandName) => {
+    const li = document.createElement('li');
+    li.textContent = bandName;
+    ul.appendChild(li);
 });
